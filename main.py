c65e8fe6af93f6871adf1ded68a4aa960a84ee3b
flaviusdolca <flaviusdolca@gmail.com>
2022-07-20 12:48:19 +0200

    refactor: Centralize replace logic

--- main.py
+++ main.py
@@ -31,71 +31,82 @@ def parse_args():
     args = args_parser.parse_args()
 
     if not args.users and not args.passwords and not args.usr_pass:
-        print("You must specify at least one list file")
-        sys.exit(1)
+        args_parser.error("You must specify at least one list file")
     if (args.users or args.passwords) and args.usr_pass:
-        print("You cannot specify user:pass file in this case. You already specified an user or password list file")
-        sys.exit(1)
+        args_parser.error(
+            "You cannot specify user:pass file in this case. You already specified an user or password list file")
     if (args.users and args.passwords):
-        print("Use user:pass list file with the option '-upw' instead of separate user and password files")
-        sys.exit(1)
+        args_parser.error(
+            "Use user:pass list file with the option '-upw' instead of separate user and password files")
     if "__USER__" in args.data and not(args.users or args.usr_pass):
-        print("__USER__ placeholder specified in request data but no users file")
-        sys.exit(1)
+        args_parser.error(
+            "__USER__ placeholder specified in request data but no users file")
     if "__PASS__" in args.data and not(args.passwords or args.usr_pass):
-        print("__PASS__ placeholder specified in request data but no password file")
-        sys.exit(1)
+        args_parser.error(
+            "__PASS__ placeholder specified in request data but no password file")
 
     cred_files = {
         'user': args.users if args.users else '',
         'pass': args.passwords if args.passwords else '',
         'user_pass': args.usr_pass if args.usr_pass else '',
     }
-
     return args.URL, args.data, cred_files
 
 
-def get_replaced_data(data, placeholder, value):
-    if value:
-        data_to_replace = data
-        data_to_replace = data_to_replace.replace(placeholder, value, 1)
-        return data_to_replace
+def get_replaced_data(data, d):
+    data_to_replace = data
+    for placeholder, value in d.items():
+        if value:
+            data_to_replace = data_to_replace.replace(placeholder, value, 1)
+    return data_to_replace
 
 
-def send_requests(url, list_file, placeholder_data, to_replace):
+def user_replace(placeholder_data, row):
+    return get_replaced_data(placeholder_data, {"__USER__": row})
+
+
+def pass_replace(placeholder_data, row):
+    return get_replaced_data(placeholder_data, {"__PASS__": row})
+
+
+def user_pass_replace(placeholder_data, row):
+    data_to_replace = placeholder_data
+    row = row.split(":", 1)
+    if len(row) == 2:
+        user = row[0]
+        passw = row[1]
+        return get_replaced_data(placeholder_data, {"__USER__": user, "__PASS__": passw})
+    else: 
+        return placeholder_data
+
+
+def send_requests(url, list_file, placeholder_data, replace_func):
     with open(list_file, "r") as lf:
         while row := lf.readline().strip():
-            print(1)
-            req_data = get_replaced_data(placeholder_data, to_replace, row)
+            req_data = replace_func(placeholder_data, row)
             try:
+                print(f"Sending: '{req_data}'")
                 res = requests.post(url, data=req_data, timeout=5)
                 print(res.content)
-            except:
+            except requests.RequestException:
                 print(
                     f"Error while trying to send POST request to '{url}' with data '{req_data}'")
 
 
 def main():
     url, placeholder_data, files = parse_args()
-    print(url, placeholder_data, files)
     if files.get("user_pass"):
-        with open(files.get("user_pass"), "r") as upf:
-            while usr_pass := upf.readline().strip():
-                data_to_replace = placeholder_data
-                usr_pass = usr_pass.split(":", 1)
-                if len(usr_pass) == 2:
-                    user = usr_pass[0]
-                    passw = usr_pass[1]
-                    data_to_replace = data_to_replace.replace(
-                        "__USER__", user, 1)
-                    data_to_replace = data_to_replace.replace(
-                        "__PASS__", passw, 1)
-                    print(data_to_replace)
+        send_requests(url, files.get("user_pass"),
+                      placeholder_data, user_pass_replace)
     elif files.get("user"):
-        send_requests(url, files.get("user"), placeholder_data, "__USER__")
+        send_requests(url, files.get("user"), placeholder_data, user_replace)
     elif files.get("pass"):
-        send_requests(url, files.get("pass"), placeholder_data, "__PASS__")
+        send_requests(url, files.get("pass"), placeholder_data, pass_replace)
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit("Interrupted by user")
+        
